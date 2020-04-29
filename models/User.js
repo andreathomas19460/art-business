86247fe8c7bf09deb72b1bd8c4cd0130e6b36fb9
Randall Burgess <randallallenburgess@gmail.com>
2020-04-29 12:01:04 -0700

    updated models to have proper information.  imported files to server.js

--- models/User.js
+++ models/User.js
@@ -4,8 +4,32 @@ const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  title: String,
-  body: String
+  
+  id: Number,
+  
+  username: {
+    type: String,
+    trim: true,
+    unique: true,
+    userCreated: {
+      type: Date,
+      default: Date.now
+    },
+    required: `username is required`
+  },
+  
+  password: {
+    type: String,
+    trim: true,
+    required: `password is required`
+    validate: [({ length }) => length >= 6, `Password should be longer.`]
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, `please enter valid email address`]
+  }
 });
 
 module.exports = mongoose.model(`User`, UserSchema);
