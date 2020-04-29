8c112cf9cc2f787af80914b7665a8dd65b1a5624
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-04-28 21:18:14 -0700

    created models directory

---
+++ models/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Art = require(`./Art`);
+const Cart = require(`./Cart`);
+const User = require(`./User`);
+
+// Exporting an object containing all of our models
+
+module.exports = {
+  Cart,
+  Art,
+  User
+};
