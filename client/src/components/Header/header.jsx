53b41a2b6fccd534259b95d08069101a51ae144c
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-05-01 12:07:39 -0700

    updated

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -8,42 +8,20 @@ class Header extends Component {
     return (
       <div>
         <header className="header navbar navbar-dark bg-dark">
-          <nav className="header navbar navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">
-              Navbar
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">
-                    Home <span className="sr-only">(current)</span>
-                  </a>
+          <nav>
+            <div class="nav-wrapper">
+              <a href="#" class="brand-logo">
+                Logo
+              </a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li>
+                  <a href="">Sass</a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Features
-                  </a>
+                <li>
+                  <a href="">Components</a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Pricing
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" href="#">
-                    Disabled
-                  </a>
+                <li>
+                  <a href="">JavaScript</a>
                 </li>
               </ul>
             </div>
