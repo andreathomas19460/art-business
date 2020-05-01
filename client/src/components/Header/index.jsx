2c83fa8f19355a8f80be0c50b99d2de9ed6c3afc
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-05-01 13:08:36 -0700

    added header code

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
+import Navbar from "../Navbar/index";
 
-class Header extends React.Component {
+class Header extends Component {
   render() {
-    return <h1>ANTHOM STUDIO</h1>;
+    return (
+      <div>
+        <header className="header navbar navbar-dark bg-dark">
+          <nav>
+            <div class="nav-wrapper">
+              <a href="#" class="brand-logo">
+                Logo
+              </a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li>
+                  <a href="">Sass</a>
+                </li>
+                <li>
+                  <a href="">Components</a>
+                </li>
+                <li>
+                  <a href="">JavaScript</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+        <div className="navbar">
+          <Navbar />
+        </div>
+      </div>
+    );
   }
 }
 
